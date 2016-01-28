6bd2af9bdbec479e9a95e9e4428e0558bf4bb717
Mitchell Seaton <meaton.2v@gmail.com>
2016-01-28 17:31:40 +0100

    handle Promises request-promise with mocha/chai

--- test/validate_pids.js
+++ test/validate_pids.js
@@ -235,7 +235,7 @@ var resolveUrlAndTest = function(ref) {
           .catch(function(err) {
             console.error('error: ' + resp.statusCode, 'ref ID: ', refID, 'record: ', record.dkclarinID);
           });
-        return req.should.eventually.have.length.above(0);
+        return expect(req).to.eventually.have.property('responseCode', 1);
       });
     });
   });
